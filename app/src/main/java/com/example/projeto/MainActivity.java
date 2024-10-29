16b3da70d8f52f9588cc63a1b3ceeb8db921278b
diogo <1230426@isep.ipp.pt>
2024-10-29 00:20:17 +0000

    update

--- app/src/main/java/com/example/projeto/MainActivity.java
+++ app/src/main/java/com/example/projeto/MainActivity.java
@@ -30,45 +30,43 @@ public class MainActivity extends AppCompatActivity {
 
         setSupportActionBar(binding.toolbar);
 
+        // Setting up NavController
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
 
-        // Configurando o Spinner
+        // Configuring the Spinner
         Spinner spinner = findViewById(R.id.spinner);
 
-        // Criando um ArrayAdapter com as opções "Definições" e "Sobre"
+        // ArrayAdapter with options "Definições" and "Sobre"
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.menu_options, android.R.layout.simple_spinner_item);
-
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        // Adicionando o título "Opções" ao Spinner (não como opção)
         spinner.setAdapter(adapter);
-
-        // Configurando o Spinner para mostrar "Opções" como um título
-        spinner.setSelection(-1); // Não selecione nada inicialmente, assim não mostra "Definições" nem "Sobre"
+        spinner.setSelection(-1); // Set to no initial selection
 
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                if (position >= 0) { // Verifica se uma opção válida foi selecionada
+                if (position >= 0) { // Checks for valid selection
                     String selectedOption = parent.getItemAtPosition(position).toString();
-                    if (selectedOption.equals("Definições")) {
+                    if ("Definições".equals(selectedOption)) {
                         Toast.makeText(MainActivity.this, "Abrindo Definições", Toast.LENGTH_SHORT).show();
-                    } else if (selectedOption.equals("Sobre")) {
+                    } else if ("Sobre".equals(selectedOption)) {
                         Toast.makeText(MainActivity.this, "Abrindo Sobre", Toast.LENGTH_SHORT).show();
                     }
+                    spinner.setSelection(-1); // Reset selection to keep "Opções" as title
                 }
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                // Não faz nada
+                // Do nothing
             }
         });
 
-        // Configurando o botão FAB
+        // Configuring the FAB button
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -81,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.layout.main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu); // Inflates the menu_main.xml you created
         return true;
     }
 
@@ -89,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
+            Toast.makeText(this, "Abrindo Configurações pelo Menu de Opções", Toast.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
